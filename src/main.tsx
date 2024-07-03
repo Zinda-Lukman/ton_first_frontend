9fc7ce7a34f1f4b29863f33cb0b5c88426a32417
Zinda Lukman <118866340+Zinda-Lukman@users.noreply.github.com>
2024-07-02 23:53:38 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import {TonConnectUIProvider} from "@tonconnect/ui-react"
 
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://github.com/Zinda-Lukman/ton_first_frontend/blob/master/manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
  
